ead81db0ad794e78b452115c3b4207c7c4364905
chris ricketts <chris.ricketts@steribar.com>
2017-09-10 16:39:04 +0100

    Minor changes

--- setup.sh
+++ setup.sh
@@ -4,9 +4,11 @@
 ln -s $HOME/.config/git/.gitconfig $HOME
 ln -s $HOME/.config/X11/.Xresources $HOME
 ln -s $HOME/.config/X11/.xinitrc $HOME
-ln -s $HOME/.config/bash/.bashrc $HOME
 
 # Install Vim Plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim \
   --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
